da4d1ff90728a1869415173f58e29768f2c2fcfe
NewennT <newenn.turbiau@gmail.com>
2025-12-10 17:29:34 +0100

    Chapitre 3

--- src/app/chapitres/chapitre-3/home.component.ts
+++ src/app/chapitres/chapitre-3/home.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
+import { filter, interval, map, Observable, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 import { ChapterTitleComponent } from '@ui-elements';
-import { CounterComponent } from './counter.component';
 
 @Component({
   selector: 'app-basic-operators',
   imports: [
-    ChapterTitleComponent,
-    CounterComponent
+    AsyncPipe,
+    ChapterTitleComponent
   ],
   template: `
     <app-chapter-title>Manipulez les émissions avec les opérateurs bas niveau</app-chapter-title>
-		<app-counter/>
+		<p class="text-xl mt-3 text-orange-600 font-bold">
+      {{ interval$ | async }}
+    </p>
   `,
   styles: ``
 })
 export class HomeComponent {
+  interval$: Observable<string> = interval(1000).pipe(
+    filter(value => value % 3 === 0),
+    map(value => value % 2 === 0 ? `${value} (pair)` : `${value} (impair)`),
+    tap(value => this.logger(value))
+  );
 
+  private logger(text: string): void{
+    console.log(`LOG: ${text}`);
+  }
 }
